0d07d08185b49242399bd3fe31a6865ac53dca0b
daniellesiamon-cpu <daniel.lesiamon@student.moringaschool.com>
2026-08-04 02:28:31 -0600

    Complete Blog Site lab

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Article from './Article';
+import Article from "./Article";
 
 function ArticleList({ posts }) {
   return (
     <main>
       {posts.map((post) => (
-        <Article 
-          key={post.id} 
-          title={post.title} 
-          date={post.date} 
-          preview={post.preview} 
-          minutes={post.minutes} 
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
         />
       ))}
     </main>
   );
 }
 
-export default ArticleList;s
+export default ArticleList;
